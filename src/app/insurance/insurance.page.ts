bd8aceb4b3d7f358d69ed4c63d8245be10a81357
kathmontalvo <kathmontalvo123@gmail.com>
2019-10-14 00:22:30 -0500

    profile and insuranceType

--- src/app/insurance/insurance.page.ts
+++ src/app/insurance/insurance.page.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: "app-profile",
-  templateUrl: "./profile.page.html",
-  styleUrls: ["./profile.page.scss"]
+  selector: "app-insurance",
+  templateUrl: "./insurance.page.html",
+  styleUrls: ["./insurance.page.scss"]
 })
-export class ProfilePage implements OnInit {
+export class InsurancePage implements OnInit {
   userInfo: object;
+  sctrType: object;
   constructor() {}
 
   ngOnInit() {
@@ -14,6 +15,9 @@ export class ProfilePage implements OnInit {
       name: "Juan Perez",
       img:
         "https://i0.wp.com/www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png?resize=256%2C256&quality=100&ssl=1"
+    };
+    this.sctrType = {
+      name: "SCTR Tipo 1"
     };
   }
 }
