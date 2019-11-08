477a6d056b9b2f396dd04145a8fcc14b460e5f92
kmontalvom <kmontalvom@ucic.pe>
2019-11-07 20:30:51 -0500

    agregando chart a home

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-home',
@@ -7,11 +8,15 @@ import { AuthService } from '../../services/auth.service';
   styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  @ViewChild('barCanvas', { static: false }) barCanvas: any;
+
   segment: string;
   user: object;
   insureTypes: object[];
   sharedInsureTypes: object[];
   show = true;
+
+  private barChart: Chart;
 
   constructor(private authService: AuthService) {}
 
@@ -68,9 +73,48 @@ export class HomePage {
         img: 'https://scontent.flim16-2.fna.fbcdn.net/v/t1.0-9/40685546_2065496340129700_6359411715586654208_n.png?_nc_cat=1&_nc_oc=AQlU35x3IxyILe7SuH4UVqxxx9_p6C_8t3TjYfsG3h7G4QFqeqlr8a3yekvdd4Serm4&_nc_ht=scontent.flim16-2.fna&oh=4b3853be3545648a44598fcd27daac06&oe=5E2CF2E1'
       }
     ];
+    // setTimeout(() => {
+    // }, 500);
+  }
+  ngAfterViewInit() {
+    this.createBarChart();
   }
 
   // segmentChanged(ev: any) {
   //   console.log("Segment changed", ev);
   // }
+  createBarChart() {
+    console.log(this.barCanvas);
+    if (this.barCanvas) {
+      this.barChart = new Chart(this.barCanvas.nativeElement, {
+        type: 'line',
+        data: {
+          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+          datasets: [
+            {
+              label: '# of Votes',
+              data: [12, 19, 3, 5, 2, 3],
+              backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)', 'rgba(255, 159, 64, 0.2)'],
+              borderColor: ['rgba(255,99,132,1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)', 'rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)'],
+              borderWidth: 1
+            }
+          ]
+        },
+        options: {
+          legend: {
+            display: false
+          },
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true
+                }
+              }
+            ]
+          }
+        }
+      });
+    }
+  }
 }
